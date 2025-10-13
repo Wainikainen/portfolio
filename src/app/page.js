13185823d4686ec78e5b84c62108e7b19ca45bb4
adam wainkainen <awainikainen@gmail.com>
2025-10-13 16:54:11 -0400

    added google font for heading

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,21 @@
+"use-client"
+import "./page.css";
+import { Raleway } from "next/font/google";
+
+const raleway = Raleway({
+  weight: ["400", "700","900"],
+  subsets: ["latin"],
+});
+
 export default function HomePage() {
-    return (
-        <main className="home">
-            <div className="home-body">
-                <h1 className="home-heading">Adam Wainikainen</h1>
-                <p className="home-txt">Software Engineer creating high-quality web applications</p>
-            </div>
-        </main>
-    )
+  return (
+    <main className="home">
+      <div className="home-body">
+        <h1 className={`home-heading ${raleway.className}`}>Adam Wainikainen</h1>
+        <p className="home-txt">
+          Software Engineer creating high-quality web applications
+        </p>
+      </div>
+    </main>
+  );
 }
