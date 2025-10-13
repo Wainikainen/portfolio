e82bc08ee8c1c716ffb11f89f9063e7ef7d314ef
adam wainkainen <awainikainen@gmail.com>
2025-10-13 16:53:39 -0400

    fixed favicon and :art:

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,32 +1,28 @@
 import "./global.css";
 import { Montserrat } from "next/font/google";
-import NavBar from "@/components/NavBar";
-import Footer from "@/components/Footer";
-
+import NavBar from "@/components/NavBar/NavBar";
+import Footer from "@/components/Footer/Footer";
 
 export const metadata = {
-    title: "Adam Wainikainen's Portfolio",
-    description: "Welcome to my portfolio",
-    icons:{
-        icon: "/public/favicon.png"
-    },
+  title: "Adam Wainikainen's Portfolio",
+  description: "Welcome to my portfolio",
+  icons: { icon: "/favicon.png" },
 };
 
 const montserrat = Montserrat({
-    weight: ["400","700"],
-    subsets: ["latin"]
+  weight: ["400", "700"],
+  subsets: ["latin"],
 });
 
-export default function DefaultLayout ({children}) {
-    return (
-        <html lang="en" className={montserrat.className} >
-            <body className="default-layout">
-                <NavBar/>
-                <main>
-                    {children}
-                </main>
-                <Footer/>
-            </body>
-        </html>
-    )
+export default function DefaultLayout({ children }) {
+  return (
+    <html lang="en" className={montserrat.className}>
+      <body className="default-layout">
+        <NavBar />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+  
 }
