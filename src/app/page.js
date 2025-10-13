ecaf89d2cf42a6ee5bc6bacacae83fdf265eb948
adam wainkainen <awainikainen@gmail.com>
2025-10-13 01:46:14 -0400

    added page xml

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+export default function HomePage() {
+    return (
+        <main className="home">
+            <div className="home-body">
+                <h1 className="home-heading">Adam Wainikainen</h1>
+                <p className="home-txt">Software Engineer creating high-quality web applications</p>
+            </div>
+        </main>
+    )
+}
