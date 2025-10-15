30ef7f132ac9afad924901ca08df547afb66f502
adam wainkainen <awainikainen@gmail.com>
2025-10-15 18:19:20 -0400

    live effect used on contact

---
+++ src/components/LiveEffect/LiveEffect.jsx
@@ -0,0 +1,13 @@
+import './LiveEffect.css';
+
+export default function LiveEffect() {
+  return (
+    <section className="live-effect">
+      <div className="wave">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </section>
+  );
+}
