2c1d15bfc91323f7f8328d921b51e2cd3ec3faca
adam wainkainen <awainikainen@gmail.com>
2025-10-15 00:08:10 -0400

    :art:

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
-"use-client"
+"use client"
 import "./page.css";
 import { Raleway } from "next/font/google";
+import Lava from "@/components/LavaEffect/LavaEffect";
 
 const raleway = Raleway({
   weight: ["400", "700","900"],
@@ -10,6 +11,7 @@ const raleway = Raleway({
 export default function HomePage() {
   return (
     <main className="home">
+      <Lava />
       <div className="home-body">
         <h1 className={`home-heading ${raleway.className}`}>Adam Wainikainen</h1>
         <p className="home-txt">
