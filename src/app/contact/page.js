8b49de7ae88eb99907c960234342134368f1ce71
adam wainkainen <awainikainen@gmail.com>
2025-10-15 18:18:28 -0400

    contact page

---
+++ src/app/contact/page.js
@@ -0,0 +1,52 @@
+import LiveEffect from "@/components/LiveEffect/LiveEffect";
+import "./page.css";
+
+export default function Contact() {
+  return (
+    <div className="contact">
+      <LiveEffect />
+      <h1 className=" contact-title">Contact Me</h1>
+      <p className="contact-sub">
+        For any business or professional inquiries, I&apos;m happy to connect.
+      </p>
+      <p className="contact-txt">
+        <a className="contact-link" href="mailto:awainikainen@gmail.com">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="contact-img"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
+            />
+          </svg>
+          awainikainen@gmail.com
+        </a>
+      </p>
+      <p className="contact-txt">
+        <a className="contact-link" href="tel:+16317070615">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="contact-img"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M14.25 9.75v-4.5m0 4.5h4.5m-4.5 0 6-6m-3 18c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z"
+            />
+          </svg>
+          631-707-0615
+        </a>
+      </p>
+    </div>
+  );
+}
