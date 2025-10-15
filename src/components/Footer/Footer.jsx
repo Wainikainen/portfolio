41fd90fb6184e9dd09380b4cb3c42a0f28ff71b2
adam wainkainen <awainikainen@gmail.com>
2025-10-15 00:07:39 -0400

    added pathname to style nav out side its global css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,9 @@
+"use client"
+
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { Raleway } from "next/font/google";
 import "./Footer.css";
+import { usePathname } from "next/navigation";
 
 const raleway = Raleway({
   weight: ["400", "700","900"],
@@ -9,8 +12,11 @@ const raleway = Raleway({
 
 const Footer = () => {
   const year = new Date().getFullYear();
+   const pathname = usePathname();
+  const footerClass = pathname === "/" ? "home-footer" : "";
+
   return (
-    <footer className="footer">
+    <footer className={`footer ${footerClass}`}>
       <p className={`footer-name ${raleway.className}`}>© {year} Adam Wainikainen</p>
       <div className="footer-links">
         <a href="https://github.com/Wainikainen" className="footer-link">
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,19 +1,40 @@
+"use client";
+
 import Link from "next/link";
 import "./NavBar.css";
 import { Raleway } from "next/font/google";
+import { usePathname } from "next/navigation";
+import * as motion from "motion/react-client";
 
 const raleway = Raleway({
-  weight: ["400", "700","900"],
+  weight: ["400", "700", "900"],
   subsets: ["latin"],
 });
 
 const NavBar = () => {
+  const pathname = usePathname();
+  const navClass =
+    pathname === "/about"
+      ? "about-nav"
+      : pathname === "/contact"
+      ? "contact-nav"
+      : "";
+
   return (
-    <nav className="navBar">
+    <nav className={`navBar ${navClass}`}>
       <ul className="navBar-links">
         <li className="navBar-link navBar-link-home ">
           <Link href="/">
-          <span className={raleway.className} >AW.</span></Link>
+            <motion.span
+              className={raleway.className}
+              style={{ display: "inline-block", cursor: "pointer" }}
+              whileHover={{ scale: 1.3, rotate: 5 }}
+              whileTap={{ scale: 0.9, rotate: -5 }}
+              transition={{ type: "spring", stiffness: 300, damping: 15 }}
+            >
+              AW.
+            </motion.span>
+          </Link>
         </li>
         <li className="navBar-link">
           <Link href="/about">About</Link>
