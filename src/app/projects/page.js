b16132d78ca2e00b1b9fde6faf9c9d0a32e2927b
adam wainkainen <awainikainen@gmail.com>
2025-10-16 22:50:18 -0400

    project page

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,9 +1,14 @@
 "use client";
 
 import "./page.css";
+import ProjectCards from "@/components/ProjectCards/ProjectCards";
 
-export default function Projects () {
-    return (
-        <></>
-    )
+export default function Projects() {
+  return (
+    <div className="projects">
+      <h1 className="projects-title">My Work</h1>
+      <p className="projects-sub">A collection of my projects</p>
+      <ProjectCards />
+    </div>
+  );
 }
