b6d8d8373d8a270ea7067319b39a5e749eff51da
adam wainkainen <awainikainen@gmail.com>
2025-10-18 03:15:52 -0400

    ,

--- src/components/ProjectCards/ProjectCards.jsx
+++ src/components/ProjectCards/ProjectCards.jsx
@@ -54,7 +54,7 @@ export default function ProjectCards() {
           className="project-card"
           initial="hidden"
           whileInView="visible"
-          viewport={{ once: false, amount: 0.2 }}
+          viewport={{ once: false, amount: 0.3 }}
         >
           <div className="project-images-scroll">
             {proj.images.map((src, i) => (
